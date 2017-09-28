2066bf4ed3b33e6952de5e42336e0058ee86feea
ei17ringo <ei17ringo@gmail.com>
2017-09-28 13:45:21 +0800

    DatePickerが乗ったViewを下に配置しておくところまで

--- sampleEditView/ViewController.swift
+++ sampleEditView/ViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//テキストフィールドのプロトコルを指定
+class ViewController: UIViewController,UITextFieldDelegate{
 
     @IBOutlet weak var formView: UIView!
     
@@ -18,11 +19,46 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var myContents: UITextView!
     
+    //datePickerが乗るView（下に隠しておく）
+    let baseView:UIView = UIView(frame: CGRect(x: 0, y: 720, width: 250, height: 250))
+    
+    //載せるdatePicker
+    let myDatePicker:UIDatePicker = UIDatePicker(frame: CGRect(x: 10, y: 20, width: 300, height: 220))
+    
+    //baseViewを閉じるためのボタン
+    let closeDButton:UIButton = UIButton(type: .system)
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //-----------------------------------
         
+        // baseViewにdatePickerを配置
+        baseView.addSubview(myDatePicker)
+        
+        // baseViewにボタンを配置（右上）
+        // self.view.frame.width 今動いているデバイスと同じ横幅
+        closeDButton.frame.origin.x = self.view.frame.width - 60
+        closeDButton.frame.origin.y = 5
+        closeDButton.frame.size.width = 50
+        closeDButton.frame.size.height = 20
+        
+        // ボタンにタイトルを設定
+        closeDButton.setTitle("Close", for: .normal)
+        baseView.addSubview(closeDButton)
+        
+        // baseViewを下にピッタリ配置、動作しているデバイスと横幅を同じする
+        baseView.frame.origin = CGPoint(x:0,y:self.view.frame.size.height)
+        baseView.frame.size = CGSize(width: self.view.frame.width, height: 250)
+        
+        // baseViewの背景色を設定
+        baseView.backgroundColor = UIColor.gray
+        
+        // baseViewをViewに追加
+        self.view.addSubview(baseView)
+        
+        //-----------------------------------
         
         //アクセサリビュー用のViewを作成
         let upView = UIView()
@@ -41,6 +77,26 @@ class ViewController: UIViewController {
         
         //タイトルを編集するためのキーボードのアクセサリービューに設定する
         myTitle.inputAccessoryView = upView
+        
+    }
+    
+    //入力開始
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        
+        print("textFieldShouldBeginEditing")
+        print(textField.tag)
+        
+        switch textField.tag {
+        case 1:
+            //タイトル
+            return true
+        case 2:
+            //日付
+            return false
+        default:
+            return true
+        }
+        
         
     }
     
